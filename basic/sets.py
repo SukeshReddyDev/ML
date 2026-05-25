09bf18de477154c67bdf1a8843778287d1572820
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-25 20:51:58 +0530

    operations in sets

--- basic/sets.py
+++ basic/sets.py
@@ -8,3 +8,34 @@ print(s)
 
 
 # OPERATIONS IN SETS
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+print(s1.union(s2))
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+print(s1.intersection(s2))
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+s1.intersection_update(s2)
+print(s1)
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+print(s1.difference(s2))
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+print(s1.symmetric_difference(s2))
+
+s1={1,2,3}
+s2={1,2,3,4,5}
+print(s1.issubset(s2))
+print(s1.issuperset(s2))
+
+s1={1,2}
+s2={3,4,5,6}
+print(s1.isdisjoint(s2))
+
